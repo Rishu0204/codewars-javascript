30b6b3064f20e9ed455906fd594fbc3f1689b7c0
rishu prasad <rishuprasad220204@gmail.com>
2023-02-19 01:13:54 +0530

    2nd challenge

--- duplicateEncoder.js
+++ duplicateEncoder.js
@@ -1,5 +1,5 @@
 // DESCRIPTION:
-// The goal of this exercise is to convert a string to a new string where each character in the new string is 
+// The goal of this exercise is to convert a string to a new string where each character in the new string is
 //"(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string.
 //Ignore capitalization when determining if a character is a duplicate.
 
@@ -10,6 +10,7 @@
 // "(( @"     =>  "))(("
 
 //Solution
+
 function duplicateEncode(word) {
   //creating an empty array
   const arr = [];
